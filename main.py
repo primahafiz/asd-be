e8ff592a3198bdeb826685c18f8f25306b4cb8dc
primahafiz <primahafiz@gmail.com>
2024-07-14 15:26:05 +0700

    feat: extract operation

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
+from argparse import ArgumentParser
 from feature import *
-from inference import *
+from constant import *
 
-# visualizeSpectrogram(convertSpectrogramToVisualizedForm(extractLogMelFromPath('./data/sample/slider6_00000029.wav')))
+if __name__ == '__main__':
+    parser = ArgumentParser()
 
-loss = inferenceIDNN(FeatureType.GAMMATONE,MachineType.SLIDER,6)
+    parser.add_argument('--type', type=str,help='Type of process',default='')
+    parser.add_argument('--model',type=str,help='Model for training',default='')
+    parser.add_argument('--feature',type=str,help='Feature used for extract and model',default='')
+    parser.add_argument('--path_to_extract',type=str,default='')
 
-print(loss)
+    args = parser.parse_args()
+
+    if args.type == 'extract':
+        resultExtract = None
+        if args.feature == FeatureType.LOGMEL.value:
+            resultExtract = extractLogMelFromPath(args.path_to_extract)
+        elif args.feature == FeatureType.GAMMATONE.value:
+            resultExtract = extractGammatoneFromPath(args.path_to_extract)
+            
+        print(resultExtract.shape)
+        print(resultExtract)
+    
